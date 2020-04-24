04d701539f8f821a3037b8642392dc5945b466b3
Scott MacDonald <scott@smacdo.com>
2020-04-23 21:48:39 -0700

    Improve command line options

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -7,63 +7,23 @@
 
 #include <iostream>
 #include <fstream>
-#include <cassert>
 
 using namespace Brainfreeze;
 
-/**
- * Summons forth helpful version information along with licensing and
- * copyright information.
- */
-void printVersionInfo()
-{
-    std::cout
-        << "Brainfreeze Interpreter " << Brainfreeze::Version << std::endl
-        << "Copyright (C) 2009-2020 Scott MacDonald." << std::endl
-        << "License GPLv2: GNU GPL version 2 "
-        << "<http://www.gnu.org/licenses/gpl.html>" << std::endl
-        << "This is free software: you are free to change it and redistribute "
-        << "it." << std::endl
-        << "There is NO WARRANTY, to the extent permitted by law." << std::endl
-        << std::endl;
-}
-
-/**
- * Prints a big chunk of descriptive text detailing the options available
- * to the user when running the brainfreeze command line interpreter. Usually
- * called when they screw up their input :)
- *
- * \param descr The program options that were configured for the command line
- */
-void printHelp()
-{
-    std::cout
-        << "Usage: brainfreeze [options] [scriptfile]" << std::endl
-        << "" << std::endl << std::endl
-        << "Report bugs to: bugs@whitespaceconsideredharmful.com" << std::endl
-        << "Website: http://whitespaceconsideredharmful.com/bf"
-        << std::endl << std::endl;
-}
-
+//---------------------------------------------------------------------------------------------------------------------
 int unguardedMain(int argc, char** argv)
 {
-    // Configure command line arguments.
-    // TODO: Support specifying input file w/out the -f flag.
-    // TODO: Support passing BF code inline
-    // TODO: Support reading from standard in / out.
-    // TODO: Support setting input / output as files.
-    // TODO: Take unbound input as file input.
     CLI::App app;
 
-    app.description("Executes brainfreeze programs (a nicer brainf*ck) from the command line");
-
-    //argparser.setAppCopyright("Brainfreeze (c) 2020 Scott MacDonald");
-
-    
-    std::map<std::string, Interpreter::EndOfStreamBehavior> EOSLookupTable({
-        {"0", Interpreter::EndOfStreamBehavior::Zero},
+    app.description("Run Brainfuck programs from the command line");
+    app.footer(
+        "Copyright (c) Scott MacDonald.\n"
+        "For support please see: https://github.com/smacdo/brainfreeze \n");
+   
+    // Command line options.
+    const std::map<std::string, Interpreter::EndOfStreamBehavior> EOSLookupTable({
         {"zero", Interpreter::EndOfStreamBehavior::Zero},
-        {"negativeone", Interpreter::EndOfStreamBehavior::NegativeOne},
+        {"negativeOne", Interpreter::EndOfStreamBehavior::NegativeOne},
         {"nochange", Interpreter::EndOfStreamBehavior::NoChange}
     });
 
@@ -76,34 +36,68 @@ int unguardedMain(int argc, char** argv)
     bool convertInputCRLF = false;      // TODO: Set default per platform.
     bool convertOutputLF = false;
 
-    // Parse command line options.
-    app.add_option("-f,--file,file", inputFilePath, "Path to Brainfreeze program")->required();
-    app.add_option("--cells", cellCount, "Number of memory cells to allocate");     // TODO: Validate.
-    app.add_set("--blockSize", blockSize, { 1, 2, 4 }, "Size of each memory cell in bytes (1, 2, or 4)");
+    app.add_option("-f,--file,file", inputFilePath)
+        ->description("Path to Brainfreeze program")
+#if _WIN32
+        ->type_name("<path\\to\\file.bf>")
+#else
+        ->type_name("<path/to/file.bf>")
+#endif
+        ->required();
 
-    app.add_option("-e,--eof", endOfStreamBehavior, "End of stream behavior")
+    app.add_option("--cells", cellCount)
+        ->description("Number of memory cells")
+        ->type_name("<number>");
+
+    app.add_set("--blockSize", blockSize, { 1, 2, 4, 8 })
+        ->description("Size of each memory cell in bytes")
+        ->type_name("<number>");
+
+    app.add_option("--eof", endOfStreamBehavior)
+        ->description("End of stream behavior")
+        ->type_name("<behavior>")
+        ->ignore_case()
+        ->ignore_underscore()
         ->transform(CLI::CheckedTransformer(EOSLookupTable, CLI::ignore_case));
 
-    app.add_flag("--convertInputCRLF,!--no-convertInputCRLF", convertInputCRLF, "TODO");
-    app.add_flag("--convertOutputLF,!--no-convertOutputLF", convertOutputLF, "TODO");
+    app.add_flag("--convertInputCRLF", convertInputCRLF)
+        ->description("Convert Windows style newlines (\\r\\n) to *nix (\\n) when reading input.")
+#if _WIN32
+        ->default_val(true)
+#else
+        ->default_val(false)
+#endif
+        ->ignore_case();
 
-    // TODO: Add ability to set null console or read from file or something.
+    app.add_flag("--convertOutputLF", convertOutputLF)
+        ->description("Convert *nix newlines (\\n) to Windows (\\r\\n) when writing output.")
+#if _WIN32
+        ->default_val(true)
+#else
+        ->default_val(false)
+#endif
+        ->ignore_case();
 
+    // Parse command line options.
     CLI11_PARSE(app, argc, argv);
 
-    // TODO: Create platform specific console.
+    // Instantiate a platform specific console handler for reading standard in and writing standard out.
+#if _WIN32
     auto console = std::make_unique<WindowsConsole>();
+#else
+#error  "TODO: Implement platform specific console"
+#endif
 
     console->setShouldConvertInputCRtoLF(convertInputCRLF);
     console->setShouldConvertOutputLFtoCRLF(convertOutputLF);
 
     // Load code from disk.
-    // TODO: Manually load the code, and then configure the compiler instead of this oneshot function.
-    // TODO: Configure interpeter with command line options.
     // TODO: Print an error if code failed to load.
     // TODO: Print errors from code along with line/column and highlighting.
     auto interpreter = Brainfreeze::Helpers::LoadFromDisk(inputFilePath);
 
+    interpreter->setCellCount(cellCount);
+    interpreter->setCellSize(blockSize);
     interpreter->setEndOfStreamBehavior(endOfStreamBehavior);
     interpreter->setConsole(std::move(console));
 
@@ -112,12 +106,12 @@ int unguardedMain(int argc, char** argv)
     return EXIT_SUCCESS;
 }
 
+//---------------------------------------------------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
-    loguru::g_stderr_verbosity = loguru::Verbosity_OFF;     // Disable writing to stderr.
+    loguru::g_stderr_verbosity = loguru::Verbosity_OFF;     // Disable writing to stderr. TODO: It still writes.
     loguru::init(argc, argv);
-    loguru::g_stderr_verbosity = loguru::Verbosity_OFF;     // Disable writing to stderr.
-    //loguru::add_file("output.log", loguru::Truncate, loguru::Verbosity_MAX);    // TODO: Make this configurable.
+
     loguru::add_file("output.log", loguru::Truncate, loguru::Verbosity_0);    // TODO: Make this configurable.
 
     try
