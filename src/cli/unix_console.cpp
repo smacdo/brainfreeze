f88d88a2e8149004b7f413ac304eab15e6c450d4
Scott MacDonald <scott@smacdo.com>
2020-04-27 22:27:07 -0700

    adding stub implementation for unbuffered unix console read/write

--- src/cli/unix_console.cpp
+++ src/cli/unix_console.cpp
@@ -2,10 +2,7 @@
 #include "unix_console.h"
 #include "bf/bf.h"
 
-#include <loguru/loguru.hpp>
-#include <array>
-#include <stdexcept>
-#include <cassert>
+#include <stdio.h>
 
 using namespace Brainfreeze;
 using namespace Brainfreeze::CommandLineApp;
@@ -13,16 +10,41 @@ using namespace Brainfreeze::CommandLineApp;
 //---------------------------------------------------------------------------------------------------------------------
 UnixConsole::UnixConsole()
 {
+    // TODO: Test if standard in, out has been redirected.
+
     // Save old terminal parameters so they can be restored when this console instance is destroyed.
+    // TODO: Don't save and modify terminal if standard in was redirected.
     if (tcgetattr(0, &oldTerminalParams_))
     {
+        // TODO: Capture unix error codes and maybe print real error messages.
         throw std::runtime_error("TODO get error and report that tcgetattr broken");
     }
+
+    // Disable echo and cannonical mode to get unbuffered character input support.
+    auto terminalParams = oldTerminalParams_;
+
+    terminalParams.c_lflag &= ~ICANON;
+    terminalParams.c_lflag &= ~ECHO;
+    terminalParams.c_cc[VMIN] = 1;
+    terminalParams.c_cc[VTIME] = 0;
+
+    if (tcsetattr(0, TCSANOW, &terminalParams) != 0)
+    {
+        // TODO: Capture unix error codes and maybe print real error messages.
+        throw std::runtime_error("something went wrong with tcsetattr");
+    }
+
+    // Disable output buffer.
+    // TODO: Only do this if standard out was not redirected.
+    setbuf(stdout, NULL);
 }
 
 //---------------------------------------------------------------------------------------------------------------------
 UnixConsole::~UnixConsole()
 {
+    // Restore old terminal parameters now that the console is being destroyed.
+    // TODO: Only do this if standard in was not redirected.
+    tcsetattr(0, TCSADRAIN, &oldTerminalParams_);
 }
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -35,8 +57,19 @@ void UnixConsole::write(char d)
 //---------------------------------------------------------------------------------------------------------------------
 char UnixConsole::read()
 {
-    // TODO: Implement stub.
-    return EOF;
+    // TODO: Handle input redirection. Maybe buffering?
+    char c = EOF;
+
+    if (::read(0, &c, 1) == -1)
+    {
+        // TODO: Capture unix error codes and maybe print real error messages.
+        throw std::runtime_error("something went wrong with read");
+    }
+
+    // TODO: Only echo if requested.
+    write(c);
+
+    return c;
 }
 
 //---------------------------------------------------------------------------------------------------------------------
