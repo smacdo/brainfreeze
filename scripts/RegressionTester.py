5f0633f81e05f17b19e5bec5091fbaa9f8083c31
Scott MacDonald <scott@smacdo.com>
2020-05-27 19:27:34 -0700

    Refactoring regression test script into classes

--- scripts/RegressionTester.py
+++ scripts/RegressionTester.py
@@ -5,58 +5,29 @@ output, standard error and exit status code which will be compared to expected v
 
 Once completed the regression test will print status of tests.
 """
-import os, sys, glob, time, struct, argparse
-from subprocess import Popen, PIPE
-
-from py import RegressionTest as RT
+import sys, argparse
+from py import RegressionTest as RT  # TODO: Improve packaging and namespacing. Break some classes into other files?
 
 # TODO: Investigate improve security by running test execution in sandboxed environment
 # TODO: Get option to be silent?
 # TODO: Check that python isn't silently changing newlines when reading from file and that the \r\n isn't showing
 #       up in test output
-# TODO: Switch .result to .exitcode
-
-#######################################################################################################################
-# Functions
-#######################################################################################################################
-def readTestDataFile(testDir, testName, dataExtension):
-    filePath = os.path.join(testDir, testName.replace('.bf', f'.{dataExtension}'))
-    fileData = open(filePath, 'rb').read() if os.path.exists(filePath) else b''
-
-    if Verbose and fileData != b'':
-        print(f'Found .{dataExtension}')
-
-    return fileData
-
-def writeFailure(outDir, result):
-    # Create output directory if it doesn't already exist
-    if not os.path.exists(outDir):
-        if Verbose:
-            print(f'Output directory does not exist, creating: {outDir}')
-        os.mkdir(outDir)
-
-    # Write out streams that have content in them
-    if result.actual.stdout != b'':
-        open(os.path.join(outDir, f'{result.name}.stdout'), 'wb').write(result.actual.stdout)
-    if result.actual.stderr != b'':
-        open(os.path.join(outDir, f'{result.name}.stderr'), 'wb').write(result.actual.stderr)
-    if result.actual.exitCode != b'':
-        open(os.path.join(outDir, f'{result.name}.result'), 'wb').write(struct.pack('i', result.actual.exitCode))
-
-    if Verbose:
-        print(f'Wrote failure details to: {outDir}')
+# TODO: Run multiple instances of the test program to improve speed
+# TODO: Add benchmark support
+# TODO: Add multiple regression directories.
+# TODO: Add xunit output format for CI
+# TODO: Detect if console supports colors and set default on that
 
 #######################################################################################################################
 # Regression test script
 #######################################################################################################################
 # Configuration
-# TODO: Detect if console supports colors and set default on that
 parser = argparse.ArgumentParser(description='Runs suite of regression tests and reports results')
 parser.add_argument('testdir', help='Path to directory containing regression tests')
 parser.add_argument('-e', '--exe', help='Path to language interpreter')
 parser.add_argument('-v', '--verbose', default=False, action='store_true', help='Print more information out while running tests')
 parser.add_argument('-o', '--outdir', default=None, help='Directory that will receive failed test run results')
-parser.add_argument('--ignoreFailures', default=False, help='If test runner should continue even if a test fails')
+parser.add_argument('--ignoreFailures', default=False, action='store_true', help='If test runner should continue even if a test fails')
 parser.add_argument('--color', default=True, help='Use text color formatting')
 
 args = parser.parse_args()
@@ -68,69 +39,22 @@ OutputDir = args.outdir
 IgnoreFailures = args.ignoreFailures
 UseColors = args.color
 
-# Create text color options
-RedColorStr = '\033[0;31m' if UseColors else ''
-GreenColorStr = '\033[0;32m' if UseColors else ''
-ResetColorStr = '\033[0m' if UseColors else ''
+# Construct program to test.
+tester = RT.TestExecutor(exePath=InterpreterExePath, verbose=Verbose)
 
-# Make sure interpreter exe path is valid
-if not os.path.isfile(InterpreterExePath):
-    sys.exit("Could not find interpreter exe")
+# Find all relevant test scenarios.
+searcher = RT.TestScenariosFinder([TestDir], shouldSort=True)
+scenarios = searcher.findAll()
 
-# Gather scripts to be tested.
-if not os.path.isdir(args.testdir):
-    sys.exit("Could not find test directory")
+# Run all tests and report results.
+outputs = RT.MultiOutputHandler()
+outputs.add(RT.ConsoleOutputHandler(useColors=UseColors))
 
-testPattern = os.path.join(args.testdir, '*.bf')
-testFiles = glob.glob(testPattern)
-testFiles.sort()
+if OutputDir is not None:
+    outputs.add(RT.PosthocOutputHandler(outputDir=OutputDir, verbose=Verbose))
 
-numberTotal = 0
-numberFailed = 0
-numberSuccess = 0
+runner = RT.TestRunner(tester, scenarios, outputs, ignoreFailures=IgnoreFailures)
+result = runner.run()
 
-# Construct regression test objects for each found test.
-Tests = []
-
-for testFilePath in testFiles:
-    test = RT.Test(InterpreterExePath, testFilePath)
-    Tests.append(test)
-
-# Run all tests
-print(f'Running {len(Tests)} regression tests...')
-testCount = 0
-
-for test in Tests:
-    # Run the test
-    print(f'[{testCount}/{len(Tests)}] {test.name}')
-    result = test.run()
-
-    testCount = testCount + 1
-
-    if result.failed:
-        numberFailed = numberFailed + 1
-
-        print(f'{RedColorStr}FAILED: {result.command}{ResetColorStr}')
-
-        for message in result.messages:
-            print(f'{RedColorStr}{message.text}{ResetColorStr}')
-            if message.hasDiff():
-                print(message.diff)
-
-        # If test failed then write results to output directory for post-hoc debugging
-        writeFailure(OutputDir, result)
-
-        if not IgnoreFailures:
-            sys.exit('Exiting because of test failure')
-    else:
-        numberSuccess = numberSuccess + 1
-
-# Write results of running tests
-# TODO: Print more detailed results
-print(f'{numberSuccess}/{numberSuccess + numberFailed} tests passed')
-
-if numberFailed > 0:
-    print(f'*** {RedColorStr}Tests failed!{ResetColorStr} Output from failed tests written to: {OutputDir} ***')
+if not result:
     sys.exit(1)
-else:
-    sys.exit(0)
--- scripts/py/RegressionTest.py
+++ scripts/py/RegressionTest.py
@@ -1,11 +1,12 @@
 """
-Self contained regression test
+A simple framework for running regression tests on console applications and reporting their results.
 """
-import os, sys
+import os, sys, glob, struct
 import difflib
 from subprocess import Popen, PIPE
 
 class TestOutput:
+    """Holds the output (expected or actual) from a test scenario."""
     stdout = None
     stderr = None
     exitCode = None
@@ -21,19 +22,173 @@ class TestOutput:
     def isSuccessfulExitCode(self):
         return self.hasExitCode and self.exitCode == 0
 
+class TestScenariosFinder:
+    """Locates scenarios to be tested according to configurable options"""
+    dirs = []
+    shouldSort = False
+
+    def __init__(self, dirs, shouldSort=False):
+        self.dirs = dirs
+        self.shouldSort = shouldSort
+
+    def findAll(self):
+        """Find all tests"""
+        scenarios = []
+
+        if self.shouldSort:
+            self.dirs.sort()
+
+        for testDir in self.dirs:
+            if not os.path.isdir(testDir):
+                sys.exit(f'Directory not found: {testDir}')
+
+            searchPattern = os.path.join(testDir, '*.bf')
+            scenarioPaths = glob.glob(searchPattern)
+
+            if (self.shouldSort):
+                scenarioPaths.sort()
+
+            for scenarioPath in scenarioPaths:
+                scenario = TestScenario(scenarioPath)
+                scenarios.append(scenario)
+        
+        return scenarios
+
+class TestRunner:
+    """Runs multiple test scenarios according to configuration options"""
+
+    def __init__(self, executor=None, scenarios=[], output=None, ignoreFailures=False):
+        self.executor = executor
+        self.scenarios = scenarios
+        self.output = output
+        self.ignoreFailures = ignoreFailures
+
+    def run(self):
+        self._onRunStart()
+        failCount = 0
+
+        for test in self.scenarios:
+            self._onTestStart(test)
+            result = self.executor.run(test)
+
+            if result.failed:
+                failCount = failCount + 1
+                self._onTestFail(test, result)
+
+                if not self.ignoreFailures:
+                    break
+            else:
+                self._onTestOk(test, result)
+
+        self._onFinish()
+        return failCount == 0
+
+    def _onRunStart(self):
+        if self.output is not None:
+            self.output.onRunStart(len(self.scenarios))
+
+    def _onTestStart(self, test):
+        if self.output is not None:
+            self.output.onTestStart(test)
+
+    def _onTestFail(self, test, result):
+        if self.output is not None:
+            self.output.onTestFail(test, result)
+
+    def _onTestOk(self, test, result):
+        if self.output is not None:
+            self.output.onTestOk(test, result)
+
+    def _onFinish(self):
+        if self.output is not None:
+            self.output.onFinish()
+
+class TestScenario:
+    """Holds information related to the running of a single test scenario / test case"""
+    exeArgs = None
+    exeArgsText = None
+    inputData = None
+    expected = None
+    verbose = False
+    command = None
+
+    def __init__(self, scriptPath):
+        # Make sure the test script exists.
+        if not os.path.isfile(scriptPath):
+            sys.exit(f'Path to regression test script is not valid: {scriptPath}')
+
+        self.path = scriptPath
+        self.scriptName = os.path.basename(self.path)
+        self.dir = os.path.dirname(self.path)
+        self.name = os.path.splitext(self.scriptName)[0]
+
+        # Load expected input/output values from disk (if provided).
+        if self.exeArgs == None:
+            self.exeArgs = self.readTestDataFile('args')
+            self.exeArgsText = self.exeArgs.decode('utf-8').rstrip()
+
+        if self.inputData == None:
+            self.inputData = self.readTestDataFile('stdin')
+
+        if self.expected == None:
+            self.expected = TestOutput(
+                self.readTestDataFile('stdout'),
+                self.readTestDataFile('stderr'),
+                self.readTestDataFile('exitcode'))
+
+    def readTestDataFile(self, dataExtension):
+        """Load test data from disk"""
+        filePath = os.path.join(self.dir, self.scriptName.replace('.bf', f'.{dataExtension}'))
+        fileData = open(filePath, 'rb').read() if os.path.exists(filePath) else b''
+
+        if self.verbose and fileData != b'':
+            print(f'Got .{dataExtension}')
+
+        return fileData
+
+class TestExecutor:
+    """Executes the program under test with a scenario and returns the result of running it"""
+    verbose : bool = False
+    exePath : str = None
+
+    def __init__(self, exePath: str, verbose: bool=False):
+        if not os.path.isfile(exePath):
+            sys.exit("Could not find path to test exe")
+
+        self.exePath = exePath
+        self.verbose = verbose
+
+    def run(self, test):
+        # Create invocation command.
+        command = f'{self.exePath} {test.path} {test.exeArgsText}'
+
+        if (self.verbose):
+            print(f'Invoke: `{command}``')
+
+        process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+        process.stdin.write(test.inputData)
+        process.stdin.close()
+
+        actual = TestOutput(process.stdout.read(), process.stderr.read(), process.wait())
+        return Result(test, test.expected, actual)
+
 class Message:
+    """Holds a test result message with optional diff text to display"""
     text = None
     diff = None
 
     def __init__(self, text, expected=None, actual=None):
         self.text = text
+        self.expected = expected
+        self.actual = actual
 
         # Perform a diff if requested
         if expected != None and actual != None:
             differ = difflib.Differ()
             difflines = differ.compare(expected.splitlines(), actual.splitlines())
-            difftext = '\n'.join(difflines)
-            self.diff = f'DIFF:\n{difftext}\nEXPECTED:\n{expected}\nACTUAL:\n{actual}\n'
+            self.diff = '\n'.join(difflines)
+            #self.diff = f'EXPECTED:\n{expected}\nACTUAL:\n{actual}\nDIFF:\n{difftext}\n'
 
     def hasDiff(self):
         return self.diff != None
@@ -69,70 +224,135 @@ class Result:
         self.failed = True
         self.messages.append(Message(text, expected, actual))
 
-class Test:
-    exeArgs = None
-    exeArgsText = None
-    inputData = None
-    expected = None
-    verbose = False
-    command = None
+class OutputHandler:
+    """Subclass to provide custom output from test runner"""
 
-    """Initialize the test"""
-    def __init__(self, exePath, scriptPath):
-        # Make sure the test script exists.
-        if not os.path.isfile(scriptPath):
-            sys.exit(f'Path to regression test script is not valid: {scriptPath}')
+    def onRunStart(self, totalTests):
+        """Called when test runner about to run tests"""
+        pass
 
-        self.exePath = exePath
-        self.path = scriptPath
-        self.scriptName = os.path.basename(self.path)
-        self.dir = os.path.dirname(self.path)
-        self.name = os.path.splitext(self.scriptName)[0]
+    def onTestStart(self, scenario):
+        """Called when a test scenario is about to be executed"""
+        pass
 
-    """Executes the test"""
-    def run(self):
+    def onTestOk(self, scenario, result):
+        """Called when test scenario runs succesfully"""
+        pass
+
+    def onTestFail(self, scenario, result):
+        """Called when test scenario fails"""
+        pass
+
+    def onFinish(self):
+        """Called when test runner finishes running"""
+        pass
+
+class MultiOutputHandler(OutputHandler):
+    """Multiplexes event to multiple output handlers"""
+
+    def __init__(self, handlers=[]):
+        self.handlers = handlers
+
+    def add(self, handler):
+        self.handlers.append(handler)
+
+    def onRunStart(self, totalTests):
+        for h in self.handlers:
+            h.onRunStart(totalTests)
+
+    def onTestStart(self, scenario):
+        for h in self.handlers:
+            h.onTestStart(scenario)
+
+    def onTestOk(self, scenario, result):
+        for h in self.handlers:
+            h.onTestOk(scenario, result)
+
+    def onTestFail(self, scenario, result):
+        for h in self.handlers:
+            h.onTestFail(scenario, result)
+
+    def onFinish(self):
+        for h in self.handlers:
+            h.onFinish()
+
+class PosthocOutputHandler(OutputHandler):
+    """Writes failed test output to an output directory for posthoc debugging"""
+    def __init__(self, outputDir, verbose=False):
+        self.outputDir = outputDir
+        self.verbose = verbose
+
+    def onTestFail(self, scenario, result):
+        # Create output directory if it doesn't already exist
+        if not os.path.exists(self.outputDir):
+            if self.verbose:
+                print(f'Output directory does not exist, creating: {self.outputDir}')
+            os.mkdir(self.outputDir)
+
+        # Write out streams that have content in them
+        if result.actual.stdout != b'':
+            open(os.path.join(self.outputDir, f'{result.name}.stdout'), 'wb').write(result.actual.stdout)
+        if result.actual.stderr != b'':
+            open(os.path.join(self.outputDir, f'{result.name}.stderr'), 'wb').write(result.actual.stderr)
+        if result.actual.exitCode != b'':
+            open(os.path.join(self.outputDir, f'{result.name}.exitcode'), 'wb').write(struct.pack('i', result.actual.exitCode))
+
         if self.verbose:
-            print(f'RUN TEST: {self.name}')
+            print(f'Wrote failure details to: {self.outputDir}')
 
-        self.loadTestData()
-        return self.runTestExe()
+class ConsoleOutputHandler(OutputHandler):
+    """Prints test runner output to the console"""
+    testCounter = 0
+    failCounter = 0
+    okCounter = 0
 
-    """Load test parameters using the name of the regression test but with different extensions"""
-    def loadTestData(self):
-        if self.exeArgs == None:
-            self.exeArgs = self.readTestDataFile('args')
-            self.exeArgsText = self.exeArgs.decode('utf-8').rstrip()
+    def __init__(self, useColors=False):
+        self.redColorStr = '\033[0;31m' if useColors else ''
+        self.greenColorStr = '\033[0;32m' if useColors else ''
+        self.resetColorStr = '\033[0m' if useColors else ''
 
-        if self.inputData == None:
-            self.inputData = self.readTestDataFile('stdin')
-        
-        if self.expected == None:
-            self.expected = TestOutput(
-                self.readTestDataFile('stdout'),
-                self.readTestDataFile('stderr'),
-                self.readTestDataFile('exitcode'))
+    def onRunStart(self, totalTests):
+        """Called when test runner about to run tests"""
+        self.totalTests = totalTests
+        print(f'Running {self.totalTests} regression tests...')
 
-    def readTestDataFile(self, dataExtension):
-        filePath = os.path.join(self.dir, self.scriptName.replace('.bf', f'.{dataExtension}'))
-        fileData = open(filePath, 'rb').read() if os.path.exists(filePath) else b''
+    def onTestStart(self, scenario):
+        """Called when a test scenario is about to be executed"""
+        self.testCounter = self.testCounter + 1
+        print(f'[{self.testCounter}/{self.totalTests}] {scenario.name}')
+        pass
+    
+    def onTestOk(self, scenario, result):
+        """Prints test runner output to the console"""
+        self.okCounter = self.okCounter + 1
+        pass
 
-        if self.verbose and fileData != b'':
-            print(f'Got .{dataExtension}')
+    def onTestFail(self, scenario, result):
+        """Called when test scenario fails"""
+        self.failCounter = self.failCounter + 1
+        print(f'{self.redColorStr}FAILED: {result.command}{self.resetColorStr}')
 
-        return fileData
+        for message in result.messages:
+            print(f'{self.redColorStr}{message.text}{self.resetColorStr}')
+            # TODO: Use console formatting to make this look nicer.
+            if message.expected is not None:
+                print("EXPECTED:")
+                print(message.expected)
 
-    """Run the test using the user provided intepreter executable. Make sure to redirect any input streamas that were
-    specified for the test."""
-    def runTestExe(self):
-        self.command = f'{self.exePath} {self.path} {self.exeArgsText}'
+            if message.actual is not None:
+                print("ACTUAL:")
+                print(message.actual)
 
-        if (self.verbose):
-            print(f'Invoke: `{self.command}``')
+            if message.diff is not None:
+                print("DIFF:")
+                print(message.diff)
 
-        process = Popen(self.command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    def onFinish(self):
+        if self.failCounter > 0:
+            print(self.redColorStr, end='')
+        else:
+            print(self.greenColorStr, end='')
 
-        process.stdin.write(self.inputData)
-        process.stdin.close()
-
-        actual = TestOutput(process.stdout.read(), process.stderr.read(), process.wait())
-        return Result(self, self.expected, actual)
+        print(f'Ran {self.testCounter} tests.', end='')
+        print(f'{self.okCounter} ok, {self.failCounter} failed, {self.totalTests} total', end='')
+        print(self.resetColorStr)
